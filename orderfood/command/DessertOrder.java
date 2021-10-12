4eb9ccb2f4ba0eac0c7b3b322276ccd092599e4f
Jackie Z <64178252+tracer-085@users.noreply.github.com>
2021-10-12 13:40:33 +0800

    命令模式-各餐厅点餐

---
+++ orderfood/command/DessertOrder.java
@@ -0,0 +1,26 @@
+package JiyiCarnival.business.orderfood.command;
+
+/**
+ * @author Zong
+ * 甜点订单
+ */
+
+public class DessertOrder implements Order
+{
+    private Cook cook;
+    private String name;
+
+    public DessertOrder(Cook cook, String name) {
+        this.cook = cook;
+        this.name = name;
+    }
+
+    /**
+     * 执行甜品订单
+     *
+     */
+    @Override
+    public void execute() {
+        cook.CookDessert(name);
+    }
+}
