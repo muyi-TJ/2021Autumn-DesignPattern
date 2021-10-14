95308a59ba5d060c136cd0d4d76e1ac64af60a3c
Jackie Z <64178252+tracer-085@users.noreply.github.com>
2021-10-14 16:18:22 +0800

    共享单车（中介者模式）

---
+++ src/main/java/JiyiCarnival/base/sharedBikes/StationOne.java
@@ -0,0 +1,12 @@
+package JiyiCarnival.base.sharedBikes;
+
+public class StationOne extends Station {
+    /**
+     * StationOne构造器
+     * @param name 站点名称
+     * @param number 站点初始共享单车的数量
+     */
+    public StationOne(String name ,Integer number){
+        super(name, number);
+    }
+}
---
+++ src/main/java/JiyiCarnival/base/sharedBikes/StationTwo.java
@@ -0,0 +1,12 @@
+package JiyiCarnival.base.sharedBikes;
+
+public class StationTwo extends Station {
+    /**
+     * StationTwo构造器
+     * @param name 站点名称
+     * @param number 站点初始共享单车的数量
+     */
+    public StationTwo(String name ,Integer number){
+        super(name, number);
+    }
+}
