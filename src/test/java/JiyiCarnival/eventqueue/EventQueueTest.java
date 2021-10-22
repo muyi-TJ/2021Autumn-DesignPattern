2c50836ccb322bacadbdbd2b53c3a607e1eae712
potassiummmm <1574996073@qq.com>
2021-10-22 15:48:39 +0800

    Update print information

--- src/test/java/JiyiCarnival/eventqueue/EventQueueTest.java
+++ src/test/java/JiyiCarnival/eventqueue/EventQueueTest.java
@@ -6,11 +6,12 @@ import org.junit.Test;
 public class EventQueueTest {
     @Test
     public void test() {
+        System.out.println("\n事件队列模式测试:");
         Broadcast broadcast = new Broadcast();
         broadcast.init();
-        broadcast.addMessage(new Message("AJTBXL"));
-        broadcast.addMessage(new Message("ajtbxl"));
-        broadcast.addMessage(new Message("安江涛别学了"));
+        broadcast.addMessage(new Message("欢迎来到济忆嘉年华"));
+        broadcast.addMessage(new Message("带小孩的游客请看管好自己的孩子"));
+        broadcast.addMessage(new Message("园区花车因天气原因暂时停止运行，不便之处，敬请谅解"));
         while (broadcast.isRunning()) {
             // Wait until all messages are announced
         }
