4e867bb5d43ced0660fb759e5d0fedc311d07258
muyi <524453007@qq.com>
2021-10-26 17:46:39 +0800

    抽象工厂模式（舞台）

---
+++ src/main/java/JiyiCarnival/entertainment/stage/abstractfactory/AbstractFactory.java
@@ -0,0 +1,22 @@
+package JiyiCarnival.entertainment.stage.abstractfactory;
+
+/**
+ * @author muyi
+ * 抽象工厂类
+ */
+public abstract class AbstractFactory
+{
+    /**
+     * 获取灯光
+     * @param lighting 灯光类型
+     * @return 灯光对象
+     */
+    public abstract Lighting getLighting(String lighting);
+
+    /**
+     * 获取物品
+     * @param item 物品种类
+     * @return 物品对象
+     */
+    public abstract Item getItem(String item);
+}
---
+++ src/main/java/JiyiCarnival/entertainment/stage/abstractfactory/Item.java
@@ -0,0 +1,13 @@
+package JiyiCarnival.entertainment.stage.abstractfactory;
+
+/**
+ * @author muyi
+ * 物品接口
+ */
+public interface Item
+{
+    /**
+     * 使用物品
+     */
+    public void useItem();
+}
