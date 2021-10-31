5371812ee11bf27e6c98b2fdf4f8caf935ead23c
Cassiee-C <76806126+Cassiee-C@users.noreply.github.com>
2021-10-31 23:50:54 +0800

    建造者模式测试（鬼屋）

---
+++ src/test/java/JiyiCarnival/bulider/HauntedHouseTest.java
@@ -0,0 +1,24 @@
+package JiyiCarnival.bulider;
+
+import JiyiCarnival.entertainment.hauntedhouse.builder.*;
+import org.junit.Test;
+
+public class HauntedHouseTest {
+    @Test
+    public void testBuilder(){
+        HauntedHouseDirector hauntedHouseDirector=new HauntedHouseDirector();
+
+        HauntedHouseBuilder hardHauntedHouseBuilder=new HardHauntedHouse();
+        hauntedHouseDirector.setHauntedHouseBuilder(hardHauntedHouseBuilder);
+        hauntedHouseDirector.construct();
+        HauntedHouse hardHauntedHouse=hardHauntedHouseBuilder.build();
+        hardHauntedHouse.play();
+
+        HauntedHouseBuilder simpleHauntedHouseBuilder=new SimpleHauntedHouse();
+        hauntedHouseDirector.setHauntedHouseBuilder(simpleHauntedHouseBuilder);
+        hauntedHouseDirector.construct();
+        HauntedHouse simpleHauntedHouse=simpleHauntedHouseBuilder.build();
+        simpleHauntedHouse.play();
+
+    }
+}
