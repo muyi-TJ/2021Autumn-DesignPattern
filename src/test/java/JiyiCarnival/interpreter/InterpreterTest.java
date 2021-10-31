7dbdb7c4b9cc2444bd87458ff98f2dc540dec74d
CHanSU <csw010420@163.com>
2021-10-31 23:06:15 +0800

    解释器输出修改

--- src/test/java/JiyiCarnival/interpreter/InterpreterTest.java
+++ src/test/java/JiyiCarnival/interpreter/InterpreterTest.java
@@ -1,5 +1,6 @@
 package JiyiCarnival.interpreter;
 
+import JiyiCarnival.Output;
 import JiyiCarnival.service.ticketPrice.interpreter.*;
 import org.junit.Test;
 
@@ -7,8 +8,8 @@ public class InterpreterTest {
     @Test
     public void test(){
         System.out.println("解释器模式测试：");
-        System.out.println("济忆嘉年华门票成人票188元，儿童票128元。夏日狂欢满三张减立减90元！");
-        System.out.println("勤勤带着16岁的表弟和他的两位大学室友一起来到售票处购买门票……");
+//        System.out.println("济忆嘉年华门票成人票188元，儿童票128元。夏日狂欢满三张减立减90元！");
+//        System.out.println("勤勤带着16岁的表弟和他的两位大学室友一起来到售票处购买门票……");
 
         Expression firstNum = new TerminalExpressionNumber(188),
                    secondNum = new TerminalExpressionNumber(3),
@@ -17,6 +18,11 @@ public class InterpreterTest {
 
         Expression finalResult = new Add(new Minus(new Multiply(firstNum,secondNum),thirdNum),forthNum);
         int result=finalResult.interpret();
-        System.out.println("勤勤等人所需要付的总票价为" + finalResult.interpret() + "元。");
+        Output.output(this.getClass().toString(),
+                "test",
+                String.valueOf(System.identityHashCode(this)),
+                "所需要付的总票价为" + finalResult.interpret() + "元。"
+        );
+//        System.out.println("所需要付的总票价为" + finalResult.interpret() + "元。");
     }
 }
