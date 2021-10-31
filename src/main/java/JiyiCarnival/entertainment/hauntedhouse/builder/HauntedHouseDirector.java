4bac103c5c1b47e36466a25578e43fc3013a4c4f
Cassiee-C <76806126+Cassiee-C@users.noreply.github.com>
2021-10-31 23:48:32 +0800

    建造者模式（修改鬼屋）

---
+++ src/main/java/JiyiCarnival/entertainment/hauntedhouse/builder/HauntedHouseDirector.java
@@ -0,0 +1,39 @@
+package JiyiCarnival.entertainment.hauntedhouse.builder;
+
+import JiyiCarnival.Output;
+
+/**
+ * 指导鬼屋规划路线类
+ */
+public class HauntedHouseDirector {
+    private HauntedHouseBuilder hauntedHouseBuilder;
+
+    /**
+     * 设置指定的建造者
+     * @param hauntedHouseBuilder 特定建造者
+     */
+    public void setHauntedHouseBuilder(HauntedHouseBuilder hauntedHouseBuilder){
+        this.hauntedHouseBuilder=hauntedHouseBuilder;
+        Output.output(
+                this.getClass().toString(),
+                "initialize",
+                String.valueOf(System.identityHashCode(this)),
+                "设置指定的建造者"
+        );
+    }
+
+    /**
+     * 指导鬼屋规划
+     */
+    public void construct(){
+        Output.output(
+                this.getClass().toString(),
+                "initialize",
+                String.valueOf(System.identityHashCode(this)),
+                "指导鬼屋路线规划（将鬼屋路线分为三段）："
+        );
+        hauntedHouseBuilder.setTheFirstPart();
+        hauntedHouseBuilder.setTheSecondPart();
+        hauntedHouseBuilder.setTheThirdPart();
+    }
+}
