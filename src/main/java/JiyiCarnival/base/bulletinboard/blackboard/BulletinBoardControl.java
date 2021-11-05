dd85364ec91ea5fa16b15407d71417025696a541
WKATZZL <TJzengle@163.com>
2021-11-05 12:59:50 +0800

    黑板模式修改

--- src/main/java/JiyiCarnival/base/bulletinboard/blackboard/BulletinBoardControl.java
+++ src/main/java/JiyiCarnival/base/bulletinboard/blackboard/BulletinBoardControl.java
@@ -14,9 +14,17 @@ public class BulletinBoardControl extends Controller
     {
         Blackboard blackboard = new Blackboard();
         Control control = new Control(blackboard);
+        control.nextSource("主题活动   开放时间");
+        control.nextSource("水上滑梯   9:00~3:00");
+        control.nextSource("电影放映   12:00~14:00");
+        control.nextSource("动物表演   15:00~17:00");
+        control.nextSource("气球之旅   16:00~17:00");
+        control.nextSource("刺激射击   18:00~22:00");
+        control.nextSource("浪漫花车   18:00~22:00");
+        control.nextSource("天真木马   18:00~22:00");
+        control.nextSource("精彩烟花   18:00~22:00");
         control.nextSource("今日游乐园设施一切正常，敬请期待！");
-        control.nextSource("今日游乐园餐厅推出新产品！");
-        //TODO 写入各项游乐活动的时间
+        control.nextSource("今日游乐园KCF餐厅推出新产品！");
         control.loopNonPrint();
         blackboard.inspectPrint();
         Clock.addTime(5);
